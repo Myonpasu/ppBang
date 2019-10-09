264f02f6ae7db36889acfdf658989aa228bde741
Gv26 <seanandrewadamson@gmail.com>
2019-10-09 01:25:54 +0100

    Rename edge progress bar descriptions

--- comparison_graph.py
+++ comparison_graph.py
@@ -32,7 +32,7 @@ graph = nx.DiGraph()
 # Form edges between all appropriate modified map pairs.
 beatmapsets = list(db.query_beatmapsets(cur_beatmapsets, statuses))
 ranked_mods = funcs.allowed_mods(playmode)
-for bms in tqdm(beatmapsets, desc='Mod Edges'):
+for bms in tqdm(beatmapsets, desc='edges (with mods)'):
     beatmaps = tuple(db.query_beatmapset_beatmaps(cur_beatmaps, bms))
     maps = db.query_maps(cur_scores, scores_table, beatmaps, ranked_mods)
     for pair in combinations(maps, 2):
@@ -42,7 +42,7 @@ for bms in tqdm(beatmapsets, desc='Mod Edges'):
 all_beatmaps = list(db.query_beatmaps(cur_beatmaps, cur_scores_single, scores_table, statuses, comparison_threshold, 0))
 num_beatmaps = len(all_beatmaps)
 num_beatmap_pairs = num_beatmaps * (num_beatmaps - 1) // 2
-for beatmap_pair in tqdm(combinations(all_beatmaps, 2), total=num_beatmap_pairs, desc='No-Mod Edges'):
+for beatmap_pair in tqdm(combinations(all_beatmaps, 2), total=num_beatmap_pairs, desc='edges (no mods)'):
     map_1, map_2 = (beatmap_pair[0], 0), (beatmap_pair[1], 0)
     funcs.form_edge(cur_scores_acc_time, cur_scores_single, scores_table, graph, map_1, map_2, comparison_threshold)
 
