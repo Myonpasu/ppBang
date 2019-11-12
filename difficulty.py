6daadcdb7c3a730e64ac7de39afcb615e09fe085
Gv26 <seanandrewadamson@gmail.com>
2019-11-12 18:48:11 +0000

    Revert to using LIL matrix for linear system matrix construction
    Improved speed at which reduced linear system matrix rows calculated:
     - Check if edge is in an edge set rather than in a graph view

--- difficulty.py
+++ difficulty.py
@@ -20,55 +20,43 @@ def process_graph(file_location):
     neighbour_count = [len(graph.in_edges(n)) + len(graph.out_edges(n)) for n in nodelist]
     adj_mat = nx.to_scipy_sparse_matrix(graph, nodelist=nodelist)
     print('Adjacency matrix calculated from graph')
-    undirected_graph = graph.to_undirected(as_view=True)
-    return undirected_graph, nodelist, neighbour_count, adj_mat
+    directed_edge_set = set(graph.out_edges)
+    edge_set = directed_edge_set.union({e[::-1] for e in directed_edge_set})
+    print('Graph edge set generated')
+    return edge_set, nodelist, neighbour_count, adj_mat
 
 
-def linear_system_row(undirected_graph, nodelist, neighbour_count, num_nodes, row_index):
+def linear_system_row(edge_set, nodelist, neighbour_count, num_nodes, row_index):
     row = np.ones(num_nodes, dtype=int)
-    zero_indices = [j for j in range(num_nodes) if undirected_graph.has_edge(nodelist[row_index], nodelist[j])]
+    zero_indices = [j for j in range(num_nodes) if (nodelist[row_index], nodelist[j]) in edge_set]
     row[zero_indices] = 0
     row[row_index] = neighbour_count[row_index] + 1
     return row
 
 
-def row_to_ijv(row_data, row_length, row_index):
-    col = []
-    data = []
-    for j in range(row_length):
-        row_element = row_data[j]
-        if row_element != 0:
-            col.append(j)
-            data.append(row_element)
-    row = [row_index] * len(col)
-    return row, col, data
-
-
-def linear_system(undirected_graph, nodelist, neighbour_count, adj_mat):
+def linear_system(edge_set, nodelist, neighbour_count, adj_mat):
     num_nodes = len(nodelist)
     max_abs_weight = max(adj_mat.max(), - adj_mat.min())
     system_vec = adj_mat.sum(axis=1).A1 - adj_mat.sum(axis=0).A1  # A - A^T
     system_vec += max_abs_weight * (num_nodes - 1)
     print('Initial linear system vector calculated')
     min_neighbour_idx = neighbour_count.index(min(neighbour_count))
-    min_neighbour_row = linear_system_row(undirected_graph, nodelist, neighbour_count, num_nodes, min_neighbour_idx)
-    row_ind, col_ind, data = row_to_ijv(min_neighbour_row, num_nodes, min_neighbour_idx)
+    min_neighbour_row = linear_system_row(edge_set, nodelist, neighbour_count, num_nodes, min_neighbour_idx)
+    system_mat = sp.lil_matrix((num_nodes, num_nodes), dtype=int)
+    system_mat[min_neighbour_idx] = min_neighbour_row
     for i in tqdm(range(num_nodes), total=num_nodes, desc='Calculating reduced linear system'):
         if i != min_neighbour_idx:
             system_vec[i] = system_vec[i] - system_vec[min_neighbour_idx]
-            row = linear_system_row(undirected_graph, nodelist, neighbour_count, num_nodes, i) - min_neighbour_row
-            new_row_ind, new_col_ind, new_data = row_to_ijv(row, num_nodes, i)
-            row_ind.extend(new_row_ind)
-            col_ind.extend(new_col_ind)
-            data.extend(new_data)
-    system_mat = sp.csr_matrix((data, (row_ind, col_ind)), shape=(num_nodes, num_nodes))
+            row = linear_system_row(edge_set, nodelist, neighbour_count, num_nodes, i) - min_neighbour_row
+            system_mat[i] = row
+    system_mat = system_mat.tocsr()
     print('Linear system prepared to solve')
     return system_mat, system_vec
 
 
 def map_difficulties(file_location):
-    undirected_graph, nodelist, neighbour_count, adj_mat = process_graph(file_location)
-    system_mat, system_vec = linear_system(undirected_graph, nodelist, neighbour_count, adj_mat)
+    edge_set, nodelist, neighbour_count, adj_mat = process_graph(file_location)
+    system_mat, system_vec = linear_system(edge_set, nodelist, neighbour_count, adj_mat)
     diffs = sparse_solve_gmres(system_mat, system_vec)
     return nodelist, diffs
 
