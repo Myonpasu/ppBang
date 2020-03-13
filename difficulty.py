4c001e50b5beefee5e3df7dfdc0c65aa5da61213
Gv26 <seanandrewadamson@gmail.com>
2020-03-13 20:26:47 +0000

    Migrate from NetworkX to graph-tool for difficulty calculation

--- difficulty.py
+++ difficulty.py
@@ -1,8 +1,15 @@
-import networkx as nx
+import graph_tool as gt
 import numpy as np
 import scipy.sparse as sp
 import scipy.sparse.linalg as spla
+from graph_tool.spectral import adjacency
 from tqdm import tqdm
+
+
+def adjacency_matrix(graph):
+    adj_mat = adjacency(graph, weight=graph.ep.weight)
+    print('Adjacency matrix calculated from graph')
+    return adj_mat
 
 
 def difficulty_dict(nodelist, diffs):
@@ -16,39 +23,42 @@ def difficulty_output(graph_filename):
     return difficulty_dict(nodelist, diffs)
 
 
-def linear_system(edge_set, nodelist, neighbour_count, adj_mat):
-    num_nodes = len(nodelist)
-    max_abs_weight = max(adj_mat.max(), - adj_mat.min())
+def linear_system(graph):
+    adj_mat = adjacency_matrix(graph)
+    neighbour_count = vertex_degrees(graph)
+    vertex_count = graph.num_vertices()
+    max_abs_weight = max(graph.ep.weight.a.max(), - graph.ep.weight.a.min())
     system_vec = adj_mat.sum(axis=0).A1 - adj_mat.sum(axis=1).A1  # Sum (A - A^T) over axis=1
-    system_vec += max_abs_weight * (num_nodes - 1)
+    system_vec += max_abs_weight * (vertex_count - 1)
     print('Initial linear system vector calculated')
     min_neighbour_idx = np.argmin(neighbour_count)
-    min_neighbour_row = linear_system_row(edge_set, nodelist, neighbour_count, num_nodes, min_neighbour_idx)
-    system_mat = sp.lil_matrix((num_nodes, num_nodes), dtype=int)
+    min_neighbour_row = linear_system_row(graph, neighbour_count, vertex_count, min_neighbour_idx)
+    system_mat = sp.lil_matrix((vertex_count, vertex_count), dtype=int)
     system_mat[min_neighbour_idx] = min_neighbour_row
-    for i in tqdm(range(num_nodes), total=num_nodes, desc='Calculating reduced linear system'):
+    for i in tqdm(range(vertex_count), total=vertex_count, desc='Calculating reduced linear system'):
         if i != min_neighbour_idx:
             system_vec[i] = system_vec[i] - system_vec[min_neighbour_idx]
-            row = linear_system_row(edge_set, nodelist, neighbour_count, num_nodes, i) - min_neighbour_row
+            row = linear_system_row(graph, neighbour_count, vertex_count, i) - min_neighbour_row
             system_mat[i] = row
     system_mat = system_mat.tocsr()
     print('Linear system prepared to solve')
     return system_mat, system_vec
 
 
-def linear_system_row(edge_set, nodelist, neighbour_count, num_nodes, row_index):
-    row = np.ones(num_nodes, dtype=int)
-    zero_indices = [j for j in range(num_nodes) if (nodelist[row_index], nodelist[j]) in edge_set]
+def linear_system_row(graph, neighbour_count, vertex_count, row_index):
+    row = np.ones(vertex_count, dtype=int)
+    zero_indices = [j for j in range(vertex_count) if graph.edge(j, row_index) or graph.edge(row_index, j)]
     row[zero_indices] = 0
     row[row_index] = neighbour_count[row_index] + 1
     return row
 
 
 def map_difficulties(file_location):
-    edge_set, nodelist, neighbour_count, adj_mat = process_graph(file_location)
-    system_mat, system_vec = linear_system(edge_set, nodelist, neighbour_count, adj_mat)
+    graph = read_graph(file_location)
+    system_mat, system_vec = linear_system(graph)
+    names = vertex_names(graph)
     diffs = sparse_solve_gmres(system_mat, system_vec)
-    return nodelist, diffs
+    return names, diffs
 
 
 def normalize_difficulties(diffs):
@@ -56,17 +66,10 @@ def normalize_difficulties(diffs):
     return diffs
 
 
-def process_graph(file_location):
-    graph = nx.read_gpickle(file_location)
+def read_graph(file_location):
+    graph = gt.load_graph(file_location, fmt='gt')
     print('Graph successfully read from file')
-    nodelist = list(graph)
-    neighbour_count = [len(graph.in_edges(n)) + len(graph.out_edges(n)) for n in nodelist]
-    adj_mat = nx.to_scipy_sparse_matrix(graph, nodelist=nodelist)
-    print('Adjacency matrix calculated from graph')
-    directed_edge_set = set(graph.out_edges)
-    edge_set = directed_edge_set.union({e[::-1] for e in directed_edge_set})
-    print('Graph edge set generated')
-    return edge_set, nodelist, neighbour_count, adj_mat
+    return graph
 
 
 def sparse_solve_gmres(a, b, precondition=False):
@@ -75,6 +78,18 @@ def sparse_solve_gmres(a, b, precondition=False):
     x, info = spla.gmres(a, b, M=precon)
     print(f'GMRES solver finished with exit code {info}')
     return x
+
+
+def vertex_degrees(graph):
+    degrees = graph.get_total_degrees(graph.get_vertices())
+    print('Vertex degrees calculated from graph')
+    return degrees
+
+
+def vertex_names(graph):
+    names = np.asarray(list(graph.vp.name))
+    print('Vertex names retrieved from graph')
+    return names
 
 
 if __name__ == '__main__':
