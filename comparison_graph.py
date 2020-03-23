d3ed61006072b7c2b5cd6659f8932fa0d4579dac
Gv26 <seanandrewadamson@gmail.com>
2020-03-23 01:07:35 +0000

    Migrate to graph-tool for comparison graph
     - Increase default player threshold for mods from 4 to 5
     - Split intra-beatmapset edge formation into a separate function
     - Update file extension to match new graph module
     - Include data dump date used in filenames
     - Update README.md to use new filenames

--- comparison_graph.py
+++ comparison_graph.py
@@ -6,8 +6,9 @@ from itertools import product
 from os import remove
 from os.path import isfile
 
-import networkx as nx
+import graph_tool as gt
 import numpy as np
+from graph_tool.generation import graph_union
 from tqdm import tqdm
 
 from database import *
@@ -29,7 +30,7 @@ def allowed_mods(playmode):
     return allowed
 
 
-def construct_graph(mode, dump_type, dump_date, statuses, threshold=30, mod_threshold=4):
+def construct_graph(mode, dump_type, dump_date, statuses, threshold=30, mod_threshold=5):
     # Preparation for database queries.
     playmode = play_mode(mode)
     beatmaps_db_loc, beatmapsets_db_loc, scores_db_loc, attribs_db_loc = db_location(mode, dump_type, dump_date)
@@ -47,64 +48,118 @@ def construct_graph(mode, dump_type, dump_date, statuses, threshold=30, mod_thre
         mod_graph_files = list(executor.map(mod_graph_wrapper, mod_graph_args))
 
     # Form edges between all appropriate intra-beatmapset map pairs.
-    intrabms_graph = nx.DiGraph()
     beatmapsets = list(query_beatmapsets(cur_beatmapsets, statuses))
-    for bms in tqdm(beatmapsets, desc='Edges (intra-beatmapset)'):
-        beatmaps = tuple(query_beatmapset_beatmaps(cur_beatmaps, bms))
-        maps = query_maps(cur_scores, scores_table, beatmaps, ranked_mods)
-        for pair in combinations(maps, 2):
-            map_1_mod, map_2_mod = pair[0][1], pair[1][1]
-            if map_1_mod != 0 and map_2_mod != 0 and map_1_mod != map_2_mod:
-                form_edge(cur_scores_acc_time, cur_scores_single, scores_table, intrabms_graph, *pair, mod_threshold)
-    intrabms_filename = f'temp_{playmode}_INTRABMS.gpickle'
-    nx.write_gpickle(intrabms_graph, intrabms_filename)
+    intrabms_graph_args = (beatmapsets, ranked_mods, mod_threshold, cur_beatmaps, cur_scores,
+                           cur_scores_acc_time, cur_scores_single, scores_table)
+    intrabms_graph = graph_intrabms(*intrabms_graph_args)
+    intrabms_filename = f'temp_{playmode}_INTRABMS.gt'
+    intrabms_graph.save(intrabms_filename, fmt='gt')
 
-    # Read component graphs from files and return composed graph.
-    mod_graph_list = [nx.read_gpickle(f) for f in mod_graph_files]
-    mod_graph_list.append(intrabms_graph)
-    print('Composing all mod graphs...')
-    graph = nx.compose_all(mod_graph_list)
+    # Combine all graphs.
+    mod_graphs = [gt.load_graph(f, fmt='gt') for f in mod_graph_files]
+    mod_graphs.append(intrabms_graph)
+    graph = graph_union_all(mod_graphs)
     return graph
 
 
-def form_edge(cur_scores_acc_time, cur_scores_single, scores_table, graph, map_1, map_2, threshold, report=False):
+def form_edge(cur_scores_acc_time, cur_scores_single, scores_tab, graph, map_1, map_2, i_1, i_2, thresh, report=False):
     """Form a directed edge between two maps if the number of common users exceeds a threshold."""
-    shared_users = tuple(query_shared_users(cur_scores_single, scores_table, *map_1, *map_2))
-    if len(shared_users) >= threshold:
-        user_accs_1, user_times_1 = query_accs_times(cur_scores_acc_time, scores_table, shared_users, *map_1)
-        user_accs_2, user_times_2 = query_accs_times(cur_scores_acc_time, scores_table, shared_users, *map_2)
+    shared_users = tuple(query_shared_users(cur_scores_single, scores_tab, *map_1, *map_2))
+    if len(shared_users) >= thresh:
+        user_accs_1, user_times_1 = query_accs_times(cur_scores_acc_time, scores_tab, shared_users, *map_1)
+        user_accs_2, user_times_2 = query_accs_times(cur_scores_acc_time, scores_tab, shared_users, *map_2)
         time_weights = timedelta_weights(user_times_1, user_times_2, weeks=8)
-        if (time_weights >= 0.125).sum() >= threshold:  # Check at least threshold score pairs were within 24 weeks.
+        if (time_weights >= 0.125).sum() >= thresh:  # Check at least threshold score pairs were within 24 weeks.
             t_stat = tstat_paired_weighted(user_accs_1, user_accs_2, time_weights)
             if not np.isnan(t_stat):
-                graph.add_edge(map_1, map_2, weight=t_stat)
+                e = graph.add_edge(i_1, i_2)
+                graph.ep.weight[e] = t_stat
             elif report:
                 print(f"Map pair has undefined t-statistic (zero variance). Skipping edge formation {(map_1, map_2)}.")
 
 
+def graph_add_names(graph, vertex_names):
+    isolated_vertices = [v for v in graph.vertices() if v.in_degree() + v.out_degree() == 0]
+    for v in graph.vertices():  # Assign name to every non-isolated vertex.
+        if v not in isolated_vertices:
+            graph.vp.name[v] = vertex_names[graph.vertex_index[v]]
+    graph.remove_vertex(isolated_vertices, fast=True)
+
+
+def graph_intrabms(beatmapsets, ranked_mods, mod_thres, cur_beatmaps, cur_scores, cur_acc_time, cur_single, scores_tab):
+    intrabms_graph = graph_spawn()
+    for bms in tqdm(beatmapsets, desc='Edges (intra-beatmapset)'):
+        bms_graph = graph_spawn()
+        beatmaps = tuple(query_beatmapset_beatmaps(cur_beatmaps, bms))
+        maps = list(query_maps(cur_scores, scores_tab, beatmaps, ranked_mods))
+        for idx_1, idx_2 in combinations(range(len(maps)), 2):
+            map_1, map_2 = maps[idx_1], maps[idx_2]
+            map_1_mod, map_2_mod = map_1[1], map_2[1]
+            if map_1_mod != 0 and map_2_mod != 0 and map_1_mod != map_2_mod:
+                form_edge(cur_acc_time, cur_single, scores_tab, bms_graph,
+                          map_1, map_2, idx_1, idx_2, mod_thres)
+        graph_add_names(bms_graph, maps)
+        intrabms_graph = graph_union_pair(intrabms_graph, bms_graph)
+    return intrabms_graph
+
+
+def graph_spawn():
+    """Return an empty directed graph with internal name and weight properties."""
+    graph = gt.Graph(directed=True)
+    graph.vp.name = graph.new_vp('vector<int>')
+    graph.ep.weight = graph.new_ep('double')
+    return graph
+
+
+def graph_union_all(graphs):
+    num_graphs = len(graphs)
+    graphs = iter(graphs)
+    u = gt.Graph(next(graphs))
+    for g in tqdm(graphs, desc='Graph union', total=num_graphs - 1):
+        u = graph_union_pair(u, g)
+    return u
+
+
+def graph_union_pair(g, h):
+    """Union of two graphs with named vertices.
+
+    Inserts h into g, which is modified, and returns g.
+    """
+    g_name_index = {g.vp.name[v]: g.vertex_index[v] for v in g.vertices()}
+    intersect_vals = [g_name_index.get(h.vp.name[v], -1) for v in h.vertices()]
+    intersect_prop = h.new_vp('unsigned long', vals=intersect_vals)
+    u = graph_union(g, h, intersection=intersect_prop, include=True, internal_props=True)
+    return u
+
+
 def mod_graph(mod, nomod_maps, num_nomod_maps, beatmaps_db, scores_db, scores_tab, mode, statuses, thresh, mod_thresh):
-    graph = nx.DiGraph()
+    graph = graph_spawn()
     cur_beatmaps, cur_scores_single, cur_scores_acc_time = db_cursors_multi(mode, beatmaps_db, scores_db)
+    nomod_indices = range(num_nomod_maps)
     if mod == 0:
-        map_pairs = combinations(nomod_maps, 2)
+        maps = nomod_maps
+        index_pairs = combinations(nomod_indices, 2)
         num_map_pairs = num_nomod_maps * (num_nomod_maps - 1) // 2
     else:
         query_maps_approved_args = cur_beatmaps, cur_scores_single, scores_tab, statuses, mod_thresh, mod
         mod_maps = query_maps_approved(*query_maps_approved_args)
+        maps = nomod_maps + mod_maps
         num_mod_maps = len(mod_maps)
-        mod_pairs = combinations(mod_maps, 2)
-        mod_nomod_pairs = product(mod_maps, nomod_maps)
-        map_pairs = chain(mod_pairs, mod_nomod_pairs)
+        num_maps = num_nomod_maps + num_mod_maps
+        mod_indices = range(num_nomod_maps, num_maps)
+        mod_pairs = combinations(mod_indices, 2)
+        mod_nomod_pairs = product(mod_indices, nomod_indices)
+        index_pairs = chain(mod_pairs, mod_nomod_pairs)
         num_map_pairs = num_mod_maps * num_nomod_maps + num_mod_maps * (num_mod_maps - 1) // 2
     mod_name = readable_mod(mod)
-    for pair in tqdm(map_pairs, desc=f'Edges ({mod_name})', total=num_map_pairs):
-        map_1, map_2 = pair[0], pair[1]
+    for idx_1, idx_2 in tqdm(index_pairs, desc=f'Edges ({mod_name})', total=num_map_pairs):  # Form appropriate edges.
+        map_1, map_2 = maps[idx_1], maps[idx_2]
         map_1_mod, map_2_mod = map_1[1], map_2[1]
         pair_threshold = mod_thresh if map_1_mod != 0 or map_2_mod != 0 else thresh
-        form_edge_args = (cur_scores_acc_time, cur_scores_single, scores_tab, graph, map_1, map_2, pair_threshold)
-        form_edge(*form_edge_args)
-    filename = f'temp_{mode}_{mod_name}.gpickle'
-    nx.write_gpickle(graph, filename)
+        form_edge(cur_scores_acc_time, cur_scores_single, scores_tab, graph, map_1, map_2, idx_1, idx_2, pair_threshold)
+    graph_add_names(graph, maps)
+    filename = f'temp_{mode}_{mod_name}.gt'
+    graph.save(filename, fmt='gt')
     return filename
 
 
@@ -167,21 +222,21 @@ if __name__ == '__main__':
     status_names = {'ranked', 'approved', 'loved'}
     # Output filename and extension.
     file_name = 'comparison_graph'
-    file_extension = 'gpickle'
+    file_extension = 'gt'
 
     # Construct comparison graph in memory.
     comparison_graph = construct_graph(game_mode, data_dump_type, data_dump_date, status_names)
     print('Comparison graph constructed')
 
-    # Format file name depending on game mode.
+    # Format file name depending on game mode and data dump date.
     if game_mode != 'standard':
         file_name += f'_{game_mode}'
-    file_name += f'.{file_extension}'
+    file_name += f'_{data_dump_date}.{file_extension}'
     # Write graph to file.
     try:
-        nx.write_gpickle(comparison_graph, file_name)
-        print(f"Graph successfully written as pickle to '{file_name}'")
+        comparison_graph.save(file_name, fmt='gt')
+        print(f"Graph successfully saved to '{file_name}'")
     except MemoryError:
         if isfile(file_name):
             remove(file_name)
-        print('MemoryError occurred when writing pickle')
+        print('MemoryError occurred while saving graph')
