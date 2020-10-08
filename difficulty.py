e934fa4563ce7b7aa6692d06dc2fa0bd9ff791f6
Gv26 <seanandrewadamson@gmail.com>
2020-10-08 01:42:47 +0100

    Replace matrix solver with linear system iteration
     - Iterated linear system is far more resource efficient
     - Difficulties now normalised about their mean instead of max

--- difficulty.py
+++ difficulty.py
@@ -1,15 +1,6 @@
 import graph_tool as gt
 import numpy as np
-import scipy.sparse as sp
-import scipy.sparse.linalg as spla
-from graph_tool.spectral import adjacency
 from tqdm import tqdm
-
-
-def adjacency_matrix(graph):
-    adj_mat = adjacency(graph, weight=graph.ep.weight)
-    print('Adjacency matrix calculated from graph')
-    return adj_mat
 
 
 def difficulty_dict(nodelist, diffs):
@@ -23,42 +14,42 @@ def difficulty_output(graph_filename):
     return difficulty_dict(nodelist, diffs)
 
 
-def linear_system(graph):
-    neighbour_counts = vertex_degrees(graph)
+def diffs_iterative(graph, epsilon=1e-9):
     vertex_count = graph.num_vertices()
-    system_vec = system_vector(graph, vertex_count)
-    min_neighbour_idx = np.argmin(neighbour_counts)
-    min_neighbour_row = linear_system_row(graph, neighbour_counts, vertex_count, min_neighbour_idx)
-    system_mat = sp.lil_matrix((vertex_count, vertex_count), dtype=int)
-    system_mat[min_neighbour_idx] = min_neighbour_row
-    for i in tqdm(range(vertex_count), total=vertex_count, desc='Calculating reduced linear system'):
-        if i != min_neighbour_idx:
-            system_vec[i] = system_vec[i] - system_vec[min_neighbour_idx]
-            row = linear_system_row(graph, neighbour_counts, vertex_count, i) - min_neighbour_row
-            system_mat[i] = row
-    system_mat = system_mat.tocsc()
-    print('Linear system prepared to solve')
-    return system_mat, system_vec
-
-
-def linear_system_row(graph, neighbour_counts, vertex_count, row_index):
-    row = np.ones(vertex_count, dtype=int)
-    zero_indices = [graph.vertex_index[v] for v in graph.vertex(row_index).all_neighbors()]  # Not necessarily sorted.
-    row[zero_indices] = 0
-    row[row_index] = neighbour_counts[row_index] + 1
-    return row
+    epsilon *= vertex_count
+    max_weight = max(graph.ep.weight_accuracy.a.max(), - graph.ep.weight_accuracy.a.min())
+    k = vertex_degrees(graph)
+    weight_in = gt.incident_edges_op(graph, 'in', 'sum', graph.ep.weight_accuracy)
+    weight_out = gt.incident_edges_op(graph, 'out', 'sum', graph.ep.weight_accuracy)
+    weight_net = weight_in.a - weight_out.a
+    weight_term = weight_net / (max_weight * (vertex_count - 1))
+    diffs = graph.new_vp('double', val=1 / vertex_count)
+    neighbor_diff_sum = graph.new_vp('double')
+    delta = 1 + epsilon
+    iteration = 1
+    print(f'Beginning iteration with epsilon = {epsilon}')
+    while delta >= epsilon:
+        for i in tqdm(graph.vertices(), total=vertex_count, desc=f'Iteration {iteration}'):
+            neighbor_diff_sum[i] = graph.get_all_neighbors(i, vprops=[diffs])[:, 1].sum()
+        new_diffs = (neighbor_diff_sum.a + weight_term) / k
+        # Unnecessary difficulty normalisation:
+        # new_diffs /= new_diffs.sum()
+        delta = np.linalg.norm(new_diffs - diffs.a, ord=1)
+        diffs.a = new_diffs
+        print(f'Completed with delta = {delta}')
+        iteration += 1
+    return diffs.a
 
 
 def map_difficulties(file_location):
     graph = read_graph(file_location)
     names = vertex_names(graph)
-    system_mat, system_vec = linear_system(graph)
-    diffs = sparse_solve_bicgstab(system_mat, system_vec)
+    diffs = diffs_iterative(graph)
     return names, diffs
 
 
 def normalize_difficulties(diffs):
-    diffs /= np.amax(diffs)
+    diffs /= np.mean(diffs)
     return diffs
 
 
@@ -66,24 +57,6 @@ def read_graph(file_location):
     graph = gt.load_graph(file_location, fmt='gt')
     print('Graph successfully read from file')
     return graph
-
-
-def sparse_solve_bicgstab(a, b, precondition=False):
-    """Solve linear equations a.x = b for x using BiCGSTAB method."""
-    precon = spla.LinearOperator(a.shape, spla.spilu(a).solve) if precondition else None
-    print('Solving linear system...')
-    x, info = spla.bicgstab(a, b, M=precon)
-    print(f'BiCGSTAB solver finished with exit code {info}')
-    return x
-
-
-def system_vector(graph, vertex_count):
-    adj_mat = adjacency_matrix(graph)
-    max_abs_weight = max(graph.ep.weight.a.max(), - graph.ep.weight.a.min())
-    system_vec = adj_mat.sum(axis=1).A1 - adj_mat.sum(axis=0).A1  # Sum (A - A^T) matrix over its columns.
-    system_vec += max_abs_weight * (vertex_count - 1)
-    print('Initial linear system vector calculated')
-    return system_vec
 
 
 def vertex_degrees(graph):
