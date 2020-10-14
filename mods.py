edd6bceeaa2235525ca8d18ef56362c3d635d921
Gv26 <seanandrewadamson@gmail.com>
2020-10-14 02:17:39 +0100

    Add function giving equivalent mods
     - Function takes enabled mods as input, and returns a tuple of all
       possible enabled mods which are equivalent to the input
     - Minor other changes for clarity

--- mods.py
+++ mods.py
@@ -11,14 +11,14 @@ class Mod(IntFlag):
     SD = 32
     DT = 64
     HT = 256
-    NC = 512  # Only set along with DoubleTime; Nightcore only gives 576.
+    NC = 512  # Only set along with DT; NC only gives 576.
     FL = 1024
     SO = 4096
-    PF = 16384  # Only set along with SuddenDeath; Perfect only gives 16416.
+    PF = 16384  # Only set along with SD; PF only gives 16416.
 
 
 def allowed_mods():
-    """Return the set of possible enabled mods, including no-mod."""
+    """Return all possible mods modulo equivalence, including no-mod."""
     mods = [Mod.EZ, Mod.HD, Mod.HR, Mod.DT, Mod.HT, Mod.FL]
     mod_powerset = chain.from_iterable(combinations(mods, r) for r in range(len(mods) + 1))
     combos = []
@@ -31,19 +31,31 @@ def allowed_mods():
     return allowed
 
 
+def equivalent_mods(mod):
+    mod = Mod(mod)
+    base_mods = [mod]
+    if Mod.DT in mod:  # DT equivalent to NC.
+        base_mods.append(mod ^ Mod.NC)
+    sd_mods = [Mod.SD | m for m in base_mods]
+    pf_mods = [Mod.PF | m for m in sd_mods]
+    equiv_mods = tuple(base_mods + sd_mods + pf_mods)
+    return equiv_mods
+
+
 def readable_mod(mod):
-    flag_mod = Mod(mod)
-    if Mod.NC in flag_mod:
-        flag_mod &= ~ Mod.DT
-    if Mod.PF in flag_mod:
-        flag_mod &= ~ Mod.SD
-    name = ','.join(m.name for m in Mod if m in flag_mod)
+    mod = Mod(mod)
+    if Mod.NC in mod:
+        mod &= ~ Mod.DT
+    if Mod.PF in mod:
+        mod &= ~ Mod.SD
+    name = ','.join(m.name for m in Mod if m in mod)
     if not name:
         name = 'NM'
     return name
 
 
 def valid_mod(mod):
+    mod = Mod(mod)
     if (Mod.EZ in mod and Mod.HR in mod) or (Mod.DT in mod and Mod.HT in mod):
         return False
     else:
