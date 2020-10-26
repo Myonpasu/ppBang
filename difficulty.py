f08312c971808caa80c2bbd270b009e58138c845
Gv26 <seanandrewadamson@gmail.com>
2020-10-26 03:16:43 +0000

    Specify mean difficulty in difficulty algorithm

--- difficulty.py
+++ difficulty.py
@@ -10,29 +10,28 @@ def difficulty_dict(nodelist, diffs):
 
 def difficulty_output(graph_filename):
     nodelist, diffs = map_difficulties(graph_filename)
-    diffs = normalize_difficulties(diffs)
     return difficulty_dict(nodelist, diffs)
 
 
-def diffs_iterative(graph, epsilon=1e-4):
+def diffs_iterative(graph, epsilon=1e-4, mean_diff=5):
     vertex_count = graph.num_vertices()
     max_weight = max(graph.ep.weight_accuracy.a.max(), - graph.ep.weight_accuracy.a.min())
     k = vertex_degrees(graph)
     weight_in = gt.incident_edges_op(graph, 'in', 'sum', graph.ep.weight_accuracy)
     weight_out = gt.incident_edges_op(graph, 'out', 'sum', graph.ep.weight_accuracy)
     weight_net = weight_in.a - weight_out.a
-    weight_term = weight_net / (max_weight * (vertex_count - 1))
-    diffs = graph.new_vp('double', val=(1 / vertex_count))
+    weight_term = weight_net * vertex_count * mean_diff / (max_weight * (vertex_count - 1))
+    diffs = graph.new_vp('double', val=mean_diff)
     neighbor_diff_sum = graph.new_vp('double')
     delta = 1 + epsilon
     iteration = 1
     print(f'Beginning iterations with epsilon = {epsilon}')
-    while delta >= epsilon:
+    while delta >= vertex_count * mean_diff * epsilon:
         for i in tqdm(graph.vertices(), total=vertex_count, desc=f'Iteration {iteration}'):
             neighbor_diff_sum[i] = graph.get_all_neighbors(i, vprops=[diffs])[:, 1].sum()
         new_diffs = (neighbor_diff_sum.a + weight_term) / k
         # Unnecessary difficulty normalisation:
-        # new_diffs /= new_diffs.sum()
+        # new_diffs *= vertex_count * mean_diff / new_diffs.sum()
         delta = np.linalg.norm(new_diffs - diffs.a, ord=1)
         diffs.a = new_diffs
         print(f'Completed with delta = {delta}')
@@ -45,11 +44,6 @@ def map_difficulties(file_location):
     names = vertex_names(graph)
     diffs = diffs_iterative(graph)
     return names, diffs
-
-
-def normalize_difficulties(diffs):
-    diffs /= np.mean(diffs)
-    return diffs
 
 
 def read_graph(file_location):
